06aabe3068e6f28f168e076f5274cfb82c6f5278
Sean Freitag <freitag.sean@gmail.com>
2013-02-06 16:13:07 -0600

    Adding a default port.

--- src/main/scala/com/gneoxsolutions/jobgrinder/ProxyMain.scala
+++ src/main/scala/com/gneoxsolutions/jobgrinder/ProxyMain.scala
@@ -5,7 +5,9 @@ import java.io.File
 
 object ProxyMain extends App {
 
-  def runApp(tapePath: String, port: Int) {
+  val defaultPort = 8080
+
+  def runApp(tapePath: String, port: Int = defaultPort) {
     val proxy: BetamaxProxy = new BetamaxProxy(tapePath, port)
 
     proxy.start()
@@ -15,15 +17,16 @@ object ProxyMain extends App {
 
   def printUsage() {
     val file = new File(this.getClass.getProtectionDomain.getCodeSource.getLocation.toURI)
-    println("Usage: ./%s {tape path} {port}".format(file.getName))
+    println("Usage: ./%s {tape path} [port=%d]".format(file.getName, defaultPort))
   }
 
   try {
     args.toList match {
+      case tapePath :: Nil         => runApp(tapePath)
       case tapePath :: port :: Nil => runApp(tapePath, port.toInt)
-      case _ => printUsage()
+      case _                       => printUsage()
     }
   } catch {
-    case e: NumberFormatException => printUsage()
+    case e: NumberFormatException  => printUsage()
   }
 }
